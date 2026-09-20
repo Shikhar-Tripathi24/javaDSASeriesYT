084864252089564a25c5d6873ad86b50867a9216
Shikhar Tripathi <st9358149@gmail.com>
2026-09-21 00:34:41 +0530

    INITIAL JAVA PROJECTS

---
+++ src/file1.java
@@ -0,0 +1,9 @@
+void main() {
+    System.out.println("Hello,World"+" jai hind");
+    System.out.print(3);
+    System.out.print("papa");
+    System.out.println("4+3");
+    System.out.println(4+3);
+
+
+}
---
+++ src/variable.java
@@ -0,0 +1,23 @@
+public class variable {
+     static void main() {
+         int totalmarks=20;
+         System.out.println(totalmarks);
+
+         //case sensitivity//
+         int weight=23;
+         int WEIGHT=34;
+         System.out.println(weight);
+         System.out.println(WEIGHT);
+
+
+//          don't use reserved keywords for assigning the variales it leads to  error
+//         int class=50;
+//         System.out.println(class);
+
+//         constant//
+//         int DAYS_IN_YEAR =365;
+
+
+//
+    }
+}
